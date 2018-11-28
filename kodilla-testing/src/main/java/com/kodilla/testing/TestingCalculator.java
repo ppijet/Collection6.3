44adc811d37706f2df2565c709df1feb33b8aad2
ppijet <ppijet@wp.pl>
2018-11-28 17:40:57 +0100

    Submoduł 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingCalculator.java
@@ -1,4 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calkulator.Calculator;
+
 public class TestingCalculator {
+
+    public static void main(String[] args) {
+
+        Calculator simpleCalculator = new Calculator();
+
+        int addResult = simpleCalculator.addAtoB(8,5);
+
+        if (addResult == 13) {
+            System.out.println("wynik dodawania jest prawidłowy");
+        } else {
+            System.out.println("wynik błędny - idź zmień baterie !!!");
+        }
+
+        int subtractResult = simpleCalculator.subtractAFromB(8,5);
+
+        if (subtractResult != 3) {
+            System.out.println("odejmowanie lepiej idzie Mozartowi");
+        } else {
+            System.out.println("brawo !!! Twój kalkulator odwalił kawał dobrej robot przy odejmowaniu");
+        }
+    }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calkulator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calkulator/Calculator.java
@@ -1,4 +1,14 @@
 package com.kodilla.testing.calkulator;
 
 public class Calculator {
+
+    public int addAtoB(int a, int b) {
+
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+
+        return a - b;
+    }
 }
