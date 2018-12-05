154ad8ab66498393118e9ceaca807afbff80f7a4
ppijet <ppijet@wp.pl>
2018-12-05 13:18:26 +0100

    submodul 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,15 +5,8 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
 
     public static void main (String [] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUserName();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
         }
     }
-}
+
 
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -3,13 +3,21 @@ package com.kodilla.testing.user;
 public class SimpleUser {
 
     private String username;
+    private String realName;
 
-    public SimpleUser(String username){
+    public SimpleUser(String username, String realName){
+
         this.username = username;
+        this.realName = realName;
     }
 
     public String getUserName() {
+
         return username;
+    }
+
+    public String getRealName() {
+        return realName;
     }
 
 }
