0cc120031880cd2d28f0934cba236d81a1c2f871
ppijet <ppijet@wp.pl>
2018-12-31 17:59:46 +0100

    submodul 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beutifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
@@ -15,5 +16,8 @@ public class StreamMain {
         poemBeautifier.beautify("Raków Częstochowa", team -> team.toUpperCase());
         poemBeautifier.beautify("Skra Częstochowa", team -> team.toLowerCase());
         poemBeautifier.beautify("Ajax Amsterdam", team -> team.replace("Amsterdam","Częstochowa"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
