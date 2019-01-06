cdf2c69897bc254bd9ae6542590d89c54a1813bb
ppijet <ppijet@wp.pl>
2019-01-06 15:26:47 +0100

    submodul 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,23 +1,21 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.beutifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[]args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        poemBeautifier.beautify("Tottenham HotSpur", team -> "◄◄◄" + team + "►►►");
-        poemBeautifier.beautify("Tottenham HotSpur", team ->  team + "®");
-        poemBeautifier.beautify("Real Madrid", team -> '♚' + team);
-        poemBeautifier.beautify("Raków Częstochowa", team -> team.toUpperCase());
-        poemBeautifier.beautify("Skra Częstochowa", team -> team.toLowerCase());
-        poemBeautifier.beautify("Ajax Amsterdam", team -> team.replace("Amsterdam","Częstochowa"));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultStringOfBooks);
     }
 }
