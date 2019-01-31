19fc7d757ec8cec027ec4b39a327dbe27111f26a
ppijet <ppijet@wp.pl>
2019-01-31 19:53:03 +0100

    poprawione 8.2 i 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -5,12 +5,19 @@ public class ExceptionHandling {
     public static void main (String[]args) {
 
         SecondChallenge secondChallenge = new SecondChallenge();
-
-        try {String text = secondChallenge.probablyIWillThrowException(2.0,1.5);
+        boolean checkStatus = false;
+        try {
+            String text = secondChallenge.probablyIWillThrowException(2.0,1.5);
+            System.out.println(text);
+            checkStatus = true;
         } catch (Exception a){
-            System.out.println(a +" caused by probablyWillThrowException ");
-        } finally { System.out.println("Remember >> Fuck PIS !!!<<");
-
+            System.out.println(a +" Invalid argument in method ");
+        } finally {
+            if (checkStatus) {
+                System.out.println("The method works properly");
+            } else {
+                System.out.println("BOOT ERROR");
+            }
         }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -10,16 +10,22 @@ public class FirstChallenge {
         return a/b;
     }
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-       try {double result = firstChallenge.divide(3,0);
-       System.out.println(result);
-        } catch (ArithmeticException a){
-           System.out.println(a + ": Divide by 0 is impossible");
-       } finally {
-           System.out.println("Remember: Math is the Queen of Science !!!");
-       }
-       }
+        boolean checkStatus = false;
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+            checkStatus = true;
+        } catch (ArithmeticException a) {
+            System.out.println(a + ": Divide by 0 is impossible");
+        } finally {
+            if (checkStatus) {
+                System.out.println("Task done correctly");
+            } else {
+                System.out.println("You should learn mathematics more");
+            }
+        }
+    }
 }
